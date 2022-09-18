dedc8a3d6b8edffd107862568e61beb01315df02
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-09-18 12:33:11 +0700

    Update logomaker.py

--- BagaskaraRobot/modules/logomaker.py
+++ BagaskaraRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ʙᴀɢᴀsᴋᴀʀᴀ ʀᴏʙᴏᴛ](https://t.me/oneupdirty_bot)",
+            caption="Logo by [Alter Base Robot](https://t.me/alterbasemusic_bot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [ʙᴀɢᴀsᴋᴀʀᴀ ʀᴏʙᴏᴛ](https://t.me/oneupdirty_bot)",
+            caption="Logo by [Alter Base Robot](https://t.me/alterbasemusic_bot)",
             force_document=False,
         )
         os.remove(flnme)
