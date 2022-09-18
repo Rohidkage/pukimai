92fb23302ca464715d40d695632b4c06d8946a21
ʙᴧɢᴧsҡᴧʀᴧ-ᴇx <93908715+ybgskr12@users.noreply.github.com>
2022-09-18 12:18:35 +0700

    Update alive.py

--- BagaskaraRobot/modules/alive.py
+++ BagaskaraRobot/modules/alive.py
@@ -9,21 +9,21 @@ from BagaskaraRobot.events import register
 from BagaskaraRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/63267910585608a6302b0.jpg"
+PHOTO = "https://telegra.ph/file/d5ec232215c11aa3e5013.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hallo Djancok!! [{event.sender.first_name}](tg://user?id={event.sender.id}), Gua ʙᴀɢᴀsᴋᴀʀᴀ ʀᴏʙᴏᴛ.** \n\n"
-  TEXT += "🔰 **Via aktif sekarang** \n\n"
-  TEXT += f"🔰 **Tuhan gua : [ʙᴧɢᴧsҡᴧʀᴧ](https://t.me/ybgskr)** \n\n"
+  TEXT = f"**Hallo !! [{event.sender.first_name}](tg://user?id={event.sender.id}), Saya Alter Base Music Robot.** \n\n"
+  TEXT += "🔰 **Via aktif : sekarang** \n\n"
+  TEXT += f"🔰 **My Owner : [Master](https://t.me/hllofubtch)** \n\n"
   TEXT += f"🔰 **Library Version :** `{telever}` \n\n"
   TEXT += f"🔰 **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"🔰 **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Makasih Yaa Djancok!! dah Mau Pake Gua Disini 🙏**"
+  TEXT += "**Makasih Yaa !! Sudah Mau Pake Saya Disini 🙏**"
   BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/loveisfuckedup"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/allfucek"),
+            Button.url("ʜᴇʟᴘ​", "https://t.me/video_tante"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/+fpF_1pbU3FNkMDhl"),
         ]
     ]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
